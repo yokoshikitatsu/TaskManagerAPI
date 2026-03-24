b08ccf7a811c35cf34ddacae6f3e98dc38d38b2c
ssociopath69@gmail.com <ssociopath69@gmail.com>
2026-03-24 20:55:13 +0600

    feat: настроен JWT Bearer в Program.cs и appsettings.json

--- TaskManagerAPI/Program.cs
+++ TaskManagerAPI/Program.cs
@@ -1,15 +1,35 @@
 using TaskManagerAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем сервисы
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options =>
-    {
-        options.SuppressModelStateInvalidFilter = true;
-    });
-
+// Добавляем контроллеры и репозиторий
+builder.Services.AddControllers();
 builder.Services.AddSingleton<ITaskRepository, TaskRepository>();
+
+// === НАСТРОЙКА JWT ===
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+    Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? "12345678912345678912345678912345"))
+    };
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,9 +39,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-app.UseAuthorization();
+app.UseAuthentication();  
+app.UseAuthorization(); 
 app.MapControllers();
 
-app.Run(); ;
+app.Run();
